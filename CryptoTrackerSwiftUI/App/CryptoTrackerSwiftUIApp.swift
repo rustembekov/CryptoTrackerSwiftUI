efc5391942f60c83b03efc2d90f122af9a7116f4
Sabyrzhan <rustembekov2003@gmail.com>
2024-06-05 17:10:23 +0500

    Ensured design for the title attributes

--- CryptoTrackerSwiftUI/App/CryptoTrackerSwiftUIApp.swift
+++ CryptoTrackerSwiftUI/App/CryptoTrackerSwiftUIApp.swift
@@ -10,10 +10,17 @@ import SwiftUI
 @main
 struct CryptoTrackerSwiftUIApp: App {
     @State private var vm = HomeViewModel()
+    
+    init(){
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor(Color.theme.accent)]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor(Color.theme.accent)]
+    }
+    
     var body: some Scene {
         WindowGroup {
             NavigationView{
                 HomeView()
+                .preferredColorScheme(.dark)
                 .navigationBarHidden(true)
             }.environmentObject(vm)
         }
